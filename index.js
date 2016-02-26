1002274319e795c4c1775050007a9ce5426eef98
Sam Dawson <sam.dawson@affinitylive.com>
2016-02-26 15:44:50 +1100

    Remvoing scrollpoint for the list of included modules because it is no londer included in the app

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // v1.0.0
 angular.module('ui.utils', [
-  'ui.scrollpoint',
+  // 'ui.scrollpoint', Not currently used within the app, could be useful in the future
   'ui.event',
   'ui.highlight',
   'ui.keypress',
